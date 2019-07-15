99e9ccf3c692427166da327b75ecfed542bf602c
rjq <jianqiang.rjq@alibaba-inc.com>
2019-07-15 17:38:54 +0800

    use cv2

--- tf_test_single.py
+++ tf_test_single.py
@@ -6,9 +6,9 @@
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 import numpy as np
-# from util import load_image
 from PIL import Image
-# import cv2
+import cv2
+
 
 def load_image(path):
     img = Image.open(path)
@@ -24,18 +24,24 @@ if __name__ == "__main__":
     graph_def.ParseFromString(f.read())
     persisted_graph = tf.import_graph_def(graph_def, name='')
     f.close()
-    
     sess = tf.InteractiveSession(graph=persisted_graph)
-    
     lr_img = tf.get_default_graph().get_tensor_by_name("input_1:0")
-    
     output = tf.get_default_graph().get_tensor_by_name("lambda_5/add:0")
+
+    lr = cv2.imread('demo/0829x4-crop.png')
+
+    if len(lr.shape) == 2:
+        lr = np.dstack((lr, lr, lr))
+    elif lr.shape[2] == 4:
+        alpha = lr[:, :, 3]
+        lr = lr[:, :, :3]
+    lr = lr[:, :, ::-1]
     
-    lr = load_image('demo/0829x4-crop.png')
     lr_feed = np.expand_dims(lr, axis=0)
     output_value = sess.run(output, feed_dict={lr_img: lr_feed})[0]
     sr = np.clip(output_value, 0, 255)
     sr = sr.astype('uint8')
-    sr = Image.fromarray(sr)
-    sr.save('download/sr.png')
+    sr = sr[:,:,::-1]
+    cv2.imwrite('download/sr.png', sr)
     print('done')
+    
